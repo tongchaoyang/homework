71382146a723c3c566245f3850ec1689ed699f7a
tongchaoyang <tongchaoyang@yahoo.com>
2018-03-21 16:31:11 -0700

    added a dynamic programming algorthm

--- Solutions.java
+++ Solutions.java
@@ -2,21 +2,24 @@ package homework;
 
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solutions {
     public static int genSequence(int s) {
-        int r = 0;
         assert s > 0;
         if (s == 1)
             return 1;
         
+        int r = 0;
         do {
             r++;
             if (s % 2 == 0) {
                 s /= 2;
             }
             else {
-                s = 3 * s + 1;
+                s *= 3;
+                s++;
             }
         } while (s > 1);
         r++;
@@ -24,7 +27,6 @@ public class Solutions {
     }
     
     public static int genSequence2(int s) {
-        int r = 0;
         assert s > 0;
         if (s == 1)
             return 1;
@@ -36,21 +38,61 @@ public class Solutions {
         }
     }
     
+    static private Map<Integer, Integer> m = new HashMap<>();
+    public static int genSequence3(int s) {
+        assert s > 0;
+        Integer i = m.get(s);
+        if (i != null)
+            return i;
+        i = genSequence2(s);
+        m.put(s, i);
+        return i;
+    }
     public static void main(String[] args) {
         int m = 0;
+        final int MAXITEMS = 100000;
         LocalTime t1 = LocalTime.now();
-        for (int i = 1; i < 4; i++) {
-            int l;
-            if (args.length > 0)
-                l = genSequence(i);
-            else 
-                l = genSequence2(i);
+        for (int i = 1; i < MAXITEMS; i++) {
+            int l = genSequence(i);
             //System.out.println("length = " + l);
             if (l > m)
                 m = l;
         }
         LocalTime t2 = LocalTime.now();
         Duration d = Duration.between(t1, t2);
+        System.out.println("Max length is " + m + " in " + d.toMillis() + "ms");
+        
+        t1 = LocalTime.now();
+        for (int i = 1; i < MAXITEMS; i++) {
+            int l = genSequence2(i);
+            //System.out.println("length = " + l);
+            if (l > m)
+                m = l;
+        }
+        t2 = LocalTime.now();
+        d = Duration.between(t1, t2);
+        System.out.println("Max length is " + m + " in " + d.toMillis() + "ms");
+        
+        t1 = LocalTime.now();
+        for (int i = 1; i < MAXITEMS; i++) {
+            int l = genSequence3(i);
+            //System.out.println("length = " + l);
+            if (l > m)
+                m = l;
+        }
+        t2 = LocalTime.now();
+        d = Duration.between(t1, t2);
+        System.out.println("Max length is " + m + " in " + d.toMillis() + "ms");
+        
+        t1 = LocalTime.now();
+        for (int i = 1; i < MAXITEMS; i++) {
+            int l = genSequence3(i);
+            //System.out.println("length = " + l);
+            if (l > m)
+                m = l;
+        }
+        t2 = LocalTime.now();
+        d = Duration.between(t1, t2);
         System.out.println("Max length is " + m + " in " + d.toMillis() + "ms");
     }
 }
