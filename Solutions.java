e784dd5bb2dee3f2ff67d1bfd2f2a6d63d8df704
tongchaoyang <tongchaoyang@yahoo.com>
2018-03-21 15:45:46 -0700

    added recursive algo
    added alternative

--- Solutions.java
+++ Solutions.java
@@ -7,6 +7,9 @@ public class Solutions {
     public static int genSequence(int s) {
         int r = 0;
         assert s > 0;
+        if (s == 1)
+            return 1;
+        
         do {
             r++;
             if (s % 2 == 0) {
@@ -16,13 +19,33 @@ public class Solutions {
                 s = 3 * s + 1;
             }
         } while (s > 1);
+        r++;
         return r;
     }
+    
+    public static int genSequence2(int s) {
+        int r = 0;
+        assert s > 0;
+        if (s == 1)
+            return 1;
+        if (s % 2 == 0) {
+            return 1 + genSequence2(s/2);
+        }
+        else {
+            return 1 + genSequence2(3 * s + 1);
+        }
+    }
+    
     public static void main(String[] args) {
         int m = 0;
         LocalTime t1 = LocalTime.now();
-        for (int i = 1; i < 1_000_000; i++) {
-            int l = genSequence(i);
+        for (int i = 1; i < 4; i++) {
+            int l;
+            if (args.length > 0)
+                l = genSequence(i);
+            else 
+                l = genSequence2(i);
+            //System.out.println("length = " + l);
             if (l > m)
                 m = l;
         }
